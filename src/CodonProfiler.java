c15a4a4e49136f8a05caf4916715d5db4600c390
wxiaoaa <wx38@duke.edu>
2018-10-24 22:21:06 -0400

    complete CodonProfiler

--- src/CodonProfiler.java
+++ src/CodonProfiler.java
@@ -16,24 +16,52 @@ public class CodonProfiler {
 		HashMap<String,Integer> map = new HashMap<>();
 		int[] ret = new int[codons.length];
 				
-		for(int k=0; k < codons.length; k++) {
-			Iterator<Character> iter = strand.iterator();
-			while (iter.hasNext()) {
-				char a = iter.next();
-				char b = 'z';           // not part of any real codon
-				char c = 'z';
-				if (iter.hasNext()) {
-					b = iter.next();
-				}
-				if (iter.hasNext()) {
-					c = iter.next();
-				}
-				String cod = ""+a+b+c;
-				if (cod.equals(codons[k])) {
-					ret[k] += 1;
-				}
+//		for(int k=0; k < codons.length; k++) {
+//			Iterator<Character> iter = strand.iterator();
+//			while (iter.hasNext()) {
+//				char a = iter.next();
+//				char b = 'z';           // not part of any real codon
+//				char c = 'z';
+//				if (iter.hasNext()) {
+//					b = iter.next();
+//				}
+//				if (iter.hasNext()) {
+//					c = iter.next();
+//				}
+//				String cod = ""+a+b+c;
+//				if (cod.equals(codons[k])) {
+//					ret[k] += 1;
+//				}
+//			}
+//		}
+
+		Iterator<Character> iter = strand.iterator();
+		while (iter.hasNext()) {
+			char a = iter.next();
+			char b = 'z';           // not part of any real codon
+			char c = 'z';
+			if (iter.hasNext()) {
+				b = iter.next();
+			}
+			if (iter.hasNext()) {
+				c = iter.next();
+			}
+			String cod = ""+a+b+c;
+			
+			if (!map.containsKey(cod)) {
+				map.put(cod, 1);
+			} else {
+				map.put(cod, map.get(cod)+1);
 			}
 		}
+		for(int k=0; k < codons.length; k++) {
+			if (map.containsKey(codons[k])) {
+				ret[k] = map.get(codons[k]);
+			} else {
+				ret[k] = 0;
+			}
+
+		}		
 		return ret;
 	}
 }
